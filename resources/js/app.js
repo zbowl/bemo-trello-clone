c73d8185e018373e7a8971b51168448d9121b7fe
zbowl <zbowl1@gmail.com>
2023-01-08 13:17:54 -0800

    updating project dependencies to include inertia, vite plugins, vuedraggable, vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import '../css/app.css';
+
+import Vue from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue';
+import VModal from 'vue-js-modal';
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        Vue.use(plugin)
+        Vue.use(VModal, { componentName: 'modal' })
+
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+})
