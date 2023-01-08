be9427b2d98b8c4f3f731f8f00faa8785072505b
zbowl <zbowl1@gmail.com>
2023-01-08 13:14:54 -0800

    BeMo 9: Api & Web Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('cards', [ApiController::class, 'cards'])->name('api.cards');
+});
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\ColumnController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +16,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/sanctum/token', [ApiController::class, 'token'])->name('sanctum.token');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/database-dump', [DashboardController::class, 'databaseDump'])->name('database-dump');
+Route::resources([
+    'columns' => ColumnController::class,
+    'cards' => CardController::class,
+]);
+Route::put('/cards/{card}/columns/{column}', [CardController::class, 'updateColumn'])->name('cards.updateColumn');
+Route::post('/columns/update', [ColumnController::class, 'updateColumns'])->name('columns-update');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::resource('cards', CardController::class);
+//Route::resource('columns', ColumnController::class);
